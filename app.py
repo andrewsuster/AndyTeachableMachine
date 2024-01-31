06f70206724f32c727ef32651c0a17a8e8f03b6d
andrewsuster <126193001+andrewsuster@users.noreply.github.com>
2024-01-31 14:50:48 -0800

    more

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+default_username = "Create Account"
+
+@app.route('/static/index')
+def home():
+    return render_template("index.html", page_title=default_username)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
